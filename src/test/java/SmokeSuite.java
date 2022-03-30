c92e8ab0dc1c5de779651e4e5c2f7f18a40522c4
ebykov <ebykov@griddynamics.com>
2022-03-30 17:03:16 +0300

    test for jenkins

--- src/test/java/SmokeSuite.java
+++ src/test/java/SmokeSuite.java
@@ -1,9 +1,8 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
 import org.junit.runners.Suite;
 
-@RunWith(JUnit4.class)
+@RunWith(Suite.class)
 
 @Suite.SuiteClasses({
         LoginTest.class,
